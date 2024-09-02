6cfcc6972986d9ab9b3872ec5c7e17d7b78a79c0
unknown <hahdddj@gmail.com>
2024-09-02 15:19:51 +0530

    two new endpoints added

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -3,11 +3,24 @@ package com.example.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/demo/api")
 public class DemoController {
-      @GetMapping("/Welcome")
-      public 
+      @GetMapping("welcome")
+      public String welcome() {
+          return "welcome to application";
+      }
       
-}
+      @GetMapping("/hello")
+      public String hello() {
+    	  return "hello ";
+    }
+      
+      @GetMapping("greet")
+      public String greet() {
+          return "greeting to application";
+      }
+      }
