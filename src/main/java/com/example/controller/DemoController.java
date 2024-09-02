9da8c4cc077e0f2da240e720778d700ed5b6e89d
unknown <hahdddj@gmail.com>
2024-09-02 10:56:50 +0530

    initial project with welcome enpoint

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo/api")
+public class DemoController {
+      @GetMapping("/Welcome")
+      public 
+      
+}
